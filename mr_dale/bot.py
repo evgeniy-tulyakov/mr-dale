a43a5baac6c76b8a52c5eb0571550ee8d359545b
evgeniy tulyakov <e.v.tulyakov1998@gmail.com>
2021-10-18 19:48:11 +0300

    Implement a more convenient search for text channels

--- mr_dale/bot.py
+++ mr_dale/bot.py
@@ -1,12 +1,10 @@
 import json
 import logging
-from typing import Optional
 
 from discord import Guild, TextChannel
 from discord.ext import commands
-from discord.utils import find
 
-from . import config
+from . import config, utilites
 
 
 
@@ -20,24 +18,22 @@ class Bot(commands.Bot):
 
     def __init__(self, *args, **kwargs):
         super(Bot, self).__init__(*args, **kwargs)
+        with open(config.UI_RESOURCES_PATH / 'startup.json', 'rb') as f:
+            self.ui_messages = json.load(f)
 
 
     async def on_ready(self):
         logger.info(f'Logged in as: {self.user.name} {self.user.id}')
-        with open(config.UI_RESOURCES_PATH / 'startup.json', 'rb') as f:
-            ui_messages = json.load(f)
         host_guild: Guild = self.guilds[0]
-        expected_bot_channel: Optional[TextChannel] = find(
-            lambda item: item.name == 'dialog', host_guild.text_channels
-        )
+        expected_bot_channel = utilites.find_channel_by_name(host_guild.text_channels, 'dialog')
         if expected_bot_channel is None:
-            await host_guild.system_channel.send(content=ui_messages['first_run_msg'], tts=True)
+            await host_guild.system_channel.send(self.ui_messages['first_run_msg'], tts=True)
             bot_channel: TextChannel = await host_guild.create_text_channel(
-                'dialog', topic=ui_messages['bot_channel']['topic']
+                'dialog', topic=self.ui_messages['bot_channel']['topic']
             )
-            await bot_channel.send(content=ui_messages['bot_channel']['first_msg'])
+            await bot_channel.send(self.ui_messages['bot_channel']['first_msg'])
         else:
-            await host_guild.system_channel.send(ui_messages['relode_msg'], tts=True)
+            await host_guild.system_channel.send(self.ui_messages['relode_msg'], tts=True)
 
 
     def run(self, token):
---
+++ mr_dale/utilites.py
@@ -0,0 +1,13 @@
+﻿from typing import Optional, Sequence
+
+from discord.abc import GuildChannel
+
+
+
+def find_channel_by_name(
+    channels: Sequence[GuildChannel], channel_name: str
+) -> Optional[GuildChannel]:
+    predicate = lambda item: item.name == channel_name
+    for channel in channels:
+        if predicate(channel):
+            return channel
